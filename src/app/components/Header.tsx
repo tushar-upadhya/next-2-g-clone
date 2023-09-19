f7d17795e5a126346e02a5c88a3a1834a120dbf7
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-09-19 20:03:43 +0530

    header done

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,25 @@
-const Header = () => {
-  return <div>Header</div>;
+import Image from "next/image";
+
+import { TbGridDots } from "react-icons/tb";
+
+const Header: React.FC = () => {
+  const ImageUrl: string =
+    "https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80";
+
+  return (
+    <div className="flex justify-end pr-4 pt-3 space-x-4 items-center">
+      <p className="hover:underline text-sm">Gmail</p>
+      <p className="hover:underline text-sm">Images</p>
+      <TbGridDots className="text-4xl hover:bg-gray-200 p-2 rounded-full" />
+      <Image
+        src={ImageUrl}
+        alt="profileImage"
+        width={30}
+        height={30}
+        className="rounded-full object-cover"
+      />
+    </div>
+  );
 };
 
 export default Header;
