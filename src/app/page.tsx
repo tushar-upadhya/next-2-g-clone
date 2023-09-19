754d88e07cffe19ab5c178af92933255f9088373
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-09-19 19:39:42 +0530

    file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      he
-    </main>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
